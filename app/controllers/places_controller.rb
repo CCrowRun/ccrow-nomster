78d6013b1a66a28193611bc24d2f3502583da963
Carrie Crow <carrie.g.crow@gmail.com>
2019-07-27 14:22:19 +0000

    Created page with edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,10 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
