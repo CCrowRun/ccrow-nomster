f2264eec34b6b7a1d36ebbe28afe85a53584efa1
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-04 16:41:40 +0000

    Updated Initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
+# config/initializers/carrierwave.rb
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_public = false
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY"],
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory = ENV["AWS_BUCKET"]
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
