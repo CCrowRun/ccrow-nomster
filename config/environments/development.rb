3843390ab49801c3c9f76f0641e6ee1c91bb76d1
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-04 17:04:21 +0000

    Set up test email to be received

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
- 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
   #Environment configuration for Devise gem
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # In the development environment your application's code is reloaded on
@@ -33,7 +43,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
