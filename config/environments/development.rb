112ee3275d475d60bb76fed2ac65790b424d380c
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-05 01:02:49 +0000

    Visual redesign of site

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config_action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :test
   #config.action_mailer.delivery_method = :smtp
 
   #config.action_mailer.smtp_settings = {
